51c075f0828ef1b691f7f8d361ccb39a27f7654b
Rich Seviora <richard.seviora@gmail.com>
2014-11-07 14:56:28 -0500

    Modifying Helper & Creating Session Form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to products_path, notice: 'Logged In!'
     else
+      flash.now[:error] = 'Incorrect User/Password'
       render :new
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # All methods placed here are automatically called as helper methods.
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
