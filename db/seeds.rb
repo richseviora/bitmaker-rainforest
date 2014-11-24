9eac5d5797535f0c79a007cda53062858db54f65
richseviora <richard.seviora@gmail.com>
2014-11-24 15:26:39 -0500

    Added Seeding Script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+10.times do
+  Product.create(name: Faker::Commerce.product_name, description:Faker::Company.catch_phrase, price_in_cents: Faker::Number.between(1,10000))
+end
+
+10.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password: '123456' )
+end
+
+100.times do
+  random_user = User.offset(rand(User.count)).first
+  random_product = Product.offset(rand(Product.count)).first
+  Review.create(user: random_user, product: random_product, comment: Faker::Hacker.say_something_smart)
+end
